d223941b4384fdd493f2fcfb72a9c73006fec272
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-11-24 13:10:05 -0500

    js function

--- public/script.js
+++ public/script.js
@@ -5,6 +5,24 @@ function matchingStreet(streetMatch, data) {
     });
 }
 
-function displayresults(i, dataset) {
-    
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 38.9897, lng: 76.9378 }
+  });
+  return map;
 }
+
+async function mainthread() {
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/izam-a38m.json')
+    const json = await data.json();
+    const input = document.querySelector('input[type="text"]');
+    input.addEventListener('input', (i) => {
+        const makematch = intMap(i, json);
+        const target = document.querySelector('.mapContainer');
+        target.innerHTML = makematch;
+    });
+}
+
+window.onload = mainThread;
