744057d102ce89af00a399d193bf28e91f81ce74
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-12-02 10:39:01 -0500

    api not working yet

--- public/script.js
+++ public/script.js
@@ -29,40 +29,39 @@ function filterchoice(i, info) {
 
 }
 
-async function main() {
-    const data = await Promise.all([
-        fetch('https://data.princegeorgescountymd.gov/resource/hnpv-i4z2.json'),
-        fetch('https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json'),
-        fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json')
-    ]);
-    console.log(data);
-    const json = await data.json();
-    const input = document.querySelector('input[type="text"]');
-    input.addEventListener('input', (i) => {
-        const showM = showMatches(i, json);
-        const target = document.querySelector('mapContainer');
-        target.innerHTML = showM;
-    });
-}
-window.onload = main;
-// let map;
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 38.9897, lng: 76.9378 }
-//   });
-//   return map;
-// }
-
-// async function mainthread() {
-//     const data = await fetch('https://data.princegeorgescountymd.gov/resource/izam-a38m.json')
+// async function main() {
+//     const data = await Promise.all([
+//         fetch('https://data.princegeorgescountymd.gov/resource/hnpv-i4z2.json'),
+//         fetch('https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json'),
+//         fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json')
+//     ]);
+//     console.log(data);
 //     const json = await data.json();
 //     const input = document.querySelector('input[type="text"]');
 //     input.addEventListener('input', (i) => {
-//         const makematch = intMap(i, json);
-//         const target = document.querySelector('.mapContainer');
-//         target.innerHTML = makematch;
+//         const showM = showMatches(i, json);
+//         const target = document.querySelector('mapContainer');
+//         target.innerHTML = showM;
 //     });
 // }
+// window.onload = main;
+// let map;
 
-// window.onload = mainThread;
+
+
+function dataret() {
+    let firstCall = fetch('https://data.princegeorgescountymd.gov/resource/hnpv-i4z2.json');
+    let secCall =  fetch('https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json');
+    let thirdCall = fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
+
+    Promise.all([firstCall, secCall, thirdCall])
+        .then(value => Promise.all(value.map(value.json())))
+        .then(finalVal => {
+            let firstCall = finalVal[0];
+            let secCall = finalVal[1];
+            let thirdCall = finalVal[2];
+            const target = document.querySelector('.mapContainer');
+            target.innerHTML = finalVal;
+        });
+}
+window.onload = dataret;
