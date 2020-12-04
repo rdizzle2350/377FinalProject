305f525d4ea43bd126b084c34080fd5b72aa4314
Israel Gonzalez <igonzal5@terpmail.umd.edu>
2020-12-04 14:32:14 -0500

    server.js now reads the api data

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
-function matchingStreet(streetMatch, data) {
+
+
+
+
+function matchingStreetCrime(streetMatch, data) {
     return data.filter((item) => {
         const reg = new RegExp(streetMatch, 'gi');
-        return item.street_address.match(reg) || item.zip.match(reg);
+        return item.street_address.match(reg);
     });
 }
 
@@ -9,7 +13,11 @@ function matchingStreet(streetMatch, data) {
 
 // }
 
-function showMatches(i, info) {
+// function calldata(getdata) {
+//     return getdata
+// }
+
+function showCrimeMatches(i, info) {
     console.log(i.target.value);
     const matching = matchingStreet(i.target.value, info);
     let placeInfoHTML = matching.map((service) => `
@@ -24,51 +32,12 @@ function showMatches(i, info) {
     return placeInfoHTML;
 }
 
-function filterchoice(i, info) {
+
+// function filterchoice(i, info) {
 
 
-}
-
-// async function main() {
-//     const data = await Promise.all([
-//         fetch('https://data.princegeorgescountymd.gov/resource/hnpv-i4z2.json'),
-//         fetch('https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json'),
-//         fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json')
-//     ]);
-//     console.log(data);
-//     const json = await data.json();
-//     const input = document.querySelector('input[type="text"]');
-//     input.addEventListener('input', (i) => {
-//         const showM = showMatches(i, json);
-//         const target = document.querySelector('mapContainer');
-//         target.innerHTML = showM;
-//     });
 // }
-// window.onload = main;
-// let map;
 
-
-
-function dataret() {
-    let firstCall = fetch('https://data.princegeorgescountymd.gov/resource/hnpv-i4z2.json');
-    let secCall =  fetch('https://data.princegeorgescountymd.gov/resource/qkn8-5mhu.json');
-    let thirdCall = fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
-
-    Promise.all([firstCall, secCall, thirdCall])
-        .then(value => Promise.all(value.map(value.json())))
-        .then(finalVal => {
-            let firstCall = finalVal[0];
-            let secCall = finalVal[1];
-            let thirdCall = finalVal[2];
-            const target = document.querySelector('.mapContainer');
-            target.innerHTML = finalVal;
-        });
-}
-window.onload = dataret;
-// window.onload = mainThread;
-function displayresults(i, dataset) {
-    
-}
 
 // Map functions
 
@@ -77,8 +46,8 @@ function initMap() {
     const colpar = { lat: 38.9897, lng: -76.9378 };
     // The map, centered at college park
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: colpar,
+        zoom: 8,
+        center: colpar,
     });
-  }
-  
+}
+
